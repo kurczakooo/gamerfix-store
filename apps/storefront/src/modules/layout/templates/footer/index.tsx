087f8fd2d0fa9a627be7995c8faa9fb3e9670a74
Damian Karwat <karwat.d@pg.com>
2026-07-09 08:29:27 +0200

    added help and company info bars to the footer

--- apps/storefront/src/modules/layout/templates/footer/index.tsx
+++ apps/storefront/src/modules/layout/templates/footer/index.tsx
@@ -3,7 +3,9 @@ import { listCollections } from "@lib/data/collections";
 import { Text, clx } from "@modules/common/components/ui";
 
 import LocalizedClientLink from "@modules/common/components/localized-client-link";
-import MedusaCTA from "@modules/layout/components/medusa-cta";
+import TikTok from "@modules/common/icons/tiktok";
+import Facebook from "@modules/common/icons/facebook";
+import Youtube from "@modules/common/icons/youtube";
 
 export default async function Footer() {
   const { collections } = await listCollections({
@@ -24,7 +26,8 @@ export default async function Footer() {
             </LocalizedClientLink>
           </div>
           <div className="text-small-regular gap-10 md:gap-x-16 grid grid-cols-2 sm:grid-cols-3">
-            {productCategories && productCategories?.length > 0 && (
+            {/* CATEGORIES LIST - UNECESSARY ATM */}
+            {/* {productCategories && productCategories?.length > 0 && (
               <div className="flex flex-col gap-y-2">
                 <span className="txt-small-plus txt-ui-fg-base">
                   Kategorie
@@ -107,50 +110,122 @@ export default async function Footer() {
                   ))}
                 </ul>
               </div>
-            )}
-            {/* <div className="flex flex-col gap-y-2">
-              <span className="txt-small-plus txt-ui-fg-base">Medusa</span>
+            )} */}
+            <div className="flex flex-col gap-y-2">
+              <span className="txt-small-plus txt-ui-fg-base">Pomoc</span>
               <ul className="grid grid-cols-1 gap-y-2 text-ui-fg-subtle txt-small">
                 <li>
+                  <LocalizedClientLink
+                    href="/contact"
+                    className="hover:text-ui-fg-base"
+                  >
+                    Kontakt
+                  </LocalizedClientLink>
+                </li>
+                <li>
+                  <LocalizedClientLink
+                    href="/customer-service"
+                    className="hover:text-ui-fg-base"
+                  >
+                    FAQ
+                  </LocalizedClientLink>
+                </li>
+                <li>
+                  <LocalizedClientLink
+                    href="/content/terms-of-use"
+                    className="hover:text-ui-fg-base"
+                  >
+                    Regulamin
+                  </LocalizedClientLink>
+                </li>
+                <li>
+                  <LocalizedClientLink
+                    href="/content/privacy-policy"
+                    className="hover:text-ui-fg-base"
+                  >
+                    Polityka prywatności
+                  </LocalizedClientLink>
+                </li>
+                <li>
+                  <LocalizedClientLink
+                    href="/content/shipping-and-payment"
+                    className="hover:text-ui-fg-base"
+                  >
+                    Dostawa i płatności
+                  </LocalizedClientLink>
+                </li>
+                <li>
+                  <LocalizedClientLink
+                    href="/content/returns-and-complaints"
+                    className="hover:text-ui-fg-base"
+                  >
+                    Zwroty i reklamacje
+                  </LocalizedClientLink>
+                </li>
+              </ul>
+            </div>
+            <div className="flex flex-col gap-y-2">
+              <span className="txt-small-plus txt-ui-fg-base">Więcej o GamerFix</span>
+              <ul className="grid grid-cols-1 gap-y-2 text-ui-fg-subtle txt-small">
+                <li>
+                  <LocalizedClientLink
+                    href="/about"
+                    className="hover:text-ui-fg-base"
+                  >
+                    O firmie
+                  </LocalizedClientLink>
+                </li>
+                <li>
                   <a
-                    href="https://github.com/medusajs"
+                    href="https://www.tiktok.com/@gamerfix.serwis"
                     target="_blank"
                     rel="noreferrer"
                     className="hover:text-ui-fg-base"
-                  >
-                    GitHub
+                    >
+                    <div className="flex items-center gap-1">
+                      <TikTok/>
+                      TikTok
+                    </div>
                   </a>
                 </li>
                 <li>
                   <a
-                    href="https://docs.medusajs.com"
+                    href="https://www.facebook.com/profile.php?id=61589715426415"
                     target="_blank"
                     rel="noreferrer"
                     className="hover:text-ui-fg-base"
                   >
-                    Documentation
+                    <div className="flex items-center gap-1">
+                      <Facebook/>
+                      Facebook
+                    </div>
                   </a>
                 </li>
                 <li>
                   <a
-                    href="https://github.com/medusajs/dtc-starter"
+                    href="https://www.youtube.com/@gamer-fix-serwis/featured"
                     target="_blank"
                     rel="noreferrer"
                     className="hover:text-ui-fg-base"
                   >
-                    Source code
+                    <div className="flex items-center gap-1">
+                      <Youtube/>
+                      YouTube
+                    </div>
                   </a>
                 </li>
               </ul>
-            </div> */}
+            </div>
+            <div className="flex flex-col gap-y-2">
+              <span className="txt-small-plus txt-ui-fg-base">REVIEWS</span>
+            </div>
           </div>
         </div>
-        {/* <div className="flex w-full mb-16 justify-between text-ui-fg-muted">
-          <Text className="txt-compact-small">
-            © {new Date().getFullYear()} Medusa Store. All rights reserved.
+        <div className="flex w-full mb-16 justify-between text-ui-fg-muted">
+          <Text className="text-xs">
+            © {new Date().getFullYear()} GamerFix
           </Text>
-          <MedusaCTA />
-        </div> */}
+        </div>
       </div>
     </footer>
   );
