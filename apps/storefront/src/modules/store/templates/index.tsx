0728be2c778bc0388dc60befbb32d6ef9f3b550a
Damian Karwat <damiankarwat04@gmail.com>
2026-07-22 11:59:03 -0400

    refactored subcategories lists component and added categories list to main shop view

--- apps/storefront/src/modules/store/templates/index.tsx
+++ apps/storefront/src/modules/store/templates/index.tsx
@@ -1,6 +1,8 @@
 import { Suspense } from "react"
 
 import { OptionValueIds } from "@lib/util/product-option-filters"
+import { HttpTypes } from "@medusajs/types"
+import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import SkeletonProductGrid from "@modules/skeletons/templates/skeleton-product-grid"
 import RefinementList from "@modules/store/components/refinement-list"
 import { SortOptions } from "@modules/store/components/refinement-list/sort-products"
@@ -12,14 +14,20 @@ const StoreTemplate = ({
   page,
   countryCode,
   optionValueIds,
+  categories,
 }: {
   sortBy?: SortOptions
   page?: string
   countryCode: string
   optionValueIds?: OptionValueIds
+  categories: HttpTypes.StoreProductCategory[]
 }) => {
   const pageNumber = page ? parseInt(page) : 1
   const sort = sortBy || "created_at"
+
+  const rootCategories = categories.filter(
+    (category) => !category.parent_category
+  )
 
   return (
     <div
@@ -31,6 +39,28 @@ const StoreTemplate = ({
         <div className="mb-8 text-2xl-semi">
           <h1 data-testid="store-page-title">Wszystkie produkty</h1>
         </div>
+        {rootCategories.length > 0 && (
+          <div className="mb-8 text-base-large">
+            <ul className="flex flex-wrap gap-2">
+              {rootCategories.map((category) => (
+                <li key={category.id}>
+                  <LocalizedClientLink
+                    className="hover:text-black"
+                    href={`/categories/${category.handle}`}
+                    data-testid="category-link"
+                  >
+                    <button
+                      className="border-ui-border-base bg-ui-bg-subtle border text-s h-10 rounded-rounded px-4 hover:shadow-elevation-card-rest transition-shadow ease-in-out duration-150"
+                      data-testid="category-button"
+                    >
+                      {category.name}
+                    </button>
+                  </LocalizedClientLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
         <Suspense fallback={<SkeletonProductGrid />}>
           <PaginatedProducts
             sortBy={sort}
