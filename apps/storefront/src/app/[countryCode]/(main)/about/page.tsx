26532fdda9c29d5b9e1fe307409aa0bf41b86635
Damian Karwat <121618421+kurczakooo@users.noreply.github.com>
2026-07-27 14:59:46 +0200

    [SUBPAGES] [STOREFRONT] Template subpages  (#6)
    * created a base FAQ subpage and template questions and answers
    
    * added a about section with text and photo gallery
    
    * added a terms of use section subpage
    
    * added a privacy policy section subpage
    
    * added a ship and pay template section subpage
    
    * added a contact section subpage with company info
    
    * added a template contact form to contact subpage
    
    * fixed typos in footer and nav
    
    * added a top of the page banner
    
    * replaced cart text with an icon on desktop, fixed code smells for nav and top bar
    
    * deleted country and language select from side menu, fixed setting and height against nav and top banner
    
    * fixed code smells and subpage links
    
    * added contact information to footer

--- apps/storefront/src/app/[countryCode]/(main)/about/page.tsx
+++ apps/storefront/src/app/[countryCode]/(main)/about/page.tsx
@@ -1,6 +1,7 @@
-import InteractiveLink from "@modules/common/components/interactive-link"
 import { Heading } from "@modules/common/components/ui"
 import { Metadata } from "next"
+import Image from "next/image"
+import about from "../../../../../data/about.json"
 
 export const metadata: Metadata = {
   title: "O firmie",
@@ -9,20 +10,52 @@ export const metadata: Metadata = {
 
 export default async function About() {
   return (
-    <div className="py-12">
-      <div className="content-container" data-testid="cart-container">
-        <div
-          className="py-48 px-2 flex flex-col justify-center items-start"
-          data-testid="empty-cart-message"
-        >
-          <Heading
-            level="h1"
-            className="flex flex-row text-3xl-regular gap-x-2 items-baseline"
-          >
-            Strona z informacjami o firmie
-          </Heading>
-          <div>
-            <InteractiveLink href="/store">Przeglądaj produkty</InteractiveLink>
+    <div className="content-container" data-testid="privacy-container">
+      <div className="py-6 px-2 flex flex-col justify-center items-start">
+        <Heading level="h1" className="text-3xl-regular">
+          {about.title}
+        </Heading>
+
+        <div className="pt-8 w-full max-w-4xl flex flex-col gap-6">
+          {about.sections.map((section) => (
+            <section key={section.content}>
+              <p className="text-base text-ui-fg-subtle leading-7">
+                {section.content}
+              </p>
+            </section>
+          ))}
+        </div>
+
+        <Heading level="h1" className="text-3xl-regular mt-8">
+          Galeria zdjęć
+        </Heading>
+
+        <div className="w-full pt-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {about.gallery.map((image, index) => (
+              <div key={index} className="group relative rounded-l">
+                <Image
+                  src={image}
+                  alt={`Galeria ${index + 1}`}
+                  width={800}
+                  height={600}
+                  className="
+                    relative
+                    z-0
+                    h-64
+                    w-full
+                    object-cover
+                    rounded-xl
+                    transition-all
+                    duration-300
+                    ease-in-out
+                    group-hover:scale-150
+                    group-hover:z-20
+                    shadow-lg
+                "
+                />
+              </div>
+            ))}
           </div>
         </div>
       </div>
