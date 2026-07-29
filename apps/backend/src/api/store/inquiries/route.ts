a94d3881c6b74b73f0ae993268592dfc17b844ba
Damian Karwat <121618421+kurczakooo@users.noreply.github.com>
2026-07-29 15:05:14 +0200

    [FEATURE] [STOREFRONT] [BACKEND] Contact inquiry mechanism implementation (#7)
    * deleted boilerplate files
    
    * adde inquiry data model, generated db migrations, created inqury module and service
    
    * adde inquiry model to configs and created the sdk
    
    * created a admin page for managing inquiries, created router, middlewares and worflows for inquiry creation and fetching
    
    * created id based routes and workflows for deletign the inquiries and updating the handled parameter
    
    * moved POST inquiry route to store route instead of admin
    
    * connected inquiry posting with storefront
    
    * deleted unused files

--- apps/backend/src/api/store/inquiries/route.ts
+++ apps/backend/src/api/store/inquiries/route.ts
@@ -2,7 +2,7 @@ import { MedusaRequest, MedusaResponse } from "@medusajs/framework/http";
 import { z } from "@medusajs/framework/zod";
 
 import { createInquiryWorkflow } from "../../../workflows/inquiries/create-inquiry";
-import { PostCreateInquiry } from "../../admin/inquiries/validators";
+import { PostCreateInquiry } from "./validators";
 
 type PostCreateInquiryType = z.infer<typeof PostCreateInquiry>;
 
