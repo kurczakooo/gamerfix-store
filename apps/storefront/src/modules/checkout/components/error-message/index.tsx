fa5ee4ceef9196798915a2a236923f7f09b88c01
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2026-08-12 06:51:58 +0200

    [STOREFRONT] fixed promo code error handling

--- apps/storefront/src/modules/checkout/components/error-message/index.tsx
+++ apps/storefront/src/modules/checkout/components/error-message/index.tsx
@@ -10,7 +10,7 @@ const ErrorMessage = ({ error, 'data-testid': dataTestid }: { error?: string | n
 
   return (
     <div className="pt-2 text-rose-500 text-small-regular" data-testid={dataTestid}>
-      <span>{errorMessageMap[error]}</span>
+      <span>{errorMessageMap[error] ?? error}</span>
     </div>
   )
 }
