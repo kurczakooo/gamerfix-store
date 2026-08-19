44ac0e617da75a1d05f3d08d459e273d0892fe8a
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2026-08-19 17:11:43 +0200

    adde favicons to page layout

--- apps/storefront/src/app/[countryCode]/(main)/layout.tsx
+++ apps/storefront/src/app/[countryCode]/(main)/layout.tsx
@@ -12,6 +12,15 @@ import TopBanner from "@modules/layout/templates/top-banner"
 
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
 }
 
 export default async function PageLayout(props: { children: React.ReactNode }) {
