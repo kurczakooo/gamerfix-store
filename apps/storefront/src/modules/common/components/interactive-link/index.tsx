3867ec9e45c36072226be7ca74d29e895ed83ca4
kurczakooo <121618421+kurczakooo@users.noreply.github.com>
2026-08-25 07:05:02 +0200

    implemented video carousel and video rail, added it to homepage

--- apps/storefront/src/modules/common/components/interactive-link/index.tsx
+++ apps/storefront/src/modules/common/components/interactive-link/index.tsx
@@ -4,15 +4,42 @@ import LocalizedClientLink from "../localized-client-link"
 type InteractiveLinkProps = {
   href: string
   children?: React.ReactNode
+  external?: boolean
   onClick?: () => void
 }
 
 const InteractiveLink = ({
   href,
   children,
+  external,
   onClick,
   ...props
 }: InteractiveLinkProps) => {
+  const content = (
+    <>
+      <Text className="text-ui-fg-interactive">{children}</Text>
+      <ArrowUpRightMini
+        className="group-hover:rotate-45 ease-in-out duration-150"
+        color="var(--fg-interactive)"
+      />
+    </>
+  )
+
+  if (external) {
+    return (
+      <a
+        className="flex gap-x-1 items-center group"
+        href={href}
+        target="_blank"
+        rel="noreferrer"
+        onClick={onClick}
+        {...props}
+      >
+        {content}
+      </a>
+    )
+  }
+
   return (
     <LocalizedClientLink
       className="flex gap-x-1 items-center group"
@@ -20,11 +47,7 @@ const InteractiveLink = ({
       onClick={onClick}
       {...props}
     >
-      <Text className="text-ui-fg-interactive">{children}</Text>
-      <ArrowUpRightMini
-        className="group-hover:rotate-45 ease-in-out duration-150"
-        color="var(--fg-interactive)"
-      />
+      {content}
     </LocalizedClientLink>
   )
 }
