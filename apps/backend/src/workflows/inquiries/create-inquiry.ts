20643eb07bc54b9d02db4006c4ef6c75c151e920
Damian Karwat <damiankarwat04@gmail.com>
2026-09-05 14:04:49 +0200

    created an email template for inquiriy notification and integrated the notifications to inquiry creation process

--- apps/backend/src/workflows/inquiries/create-inquiry.ts
+++ apps/backend/src/workflows/inquiries/create-inquiry.ts
@@ -3,9 +3,13 @@ import {
   StepResponse,
   createWorkflow,
   WorkflowResponse,
+  transform,
 } from "@medusajs/framework/workflows-sdk";
+import { sendNotificationsStep } from "@medusajs/medusa/core-flows";
 import { INQUIRY_MODULE } from "../../modules/inquiry";
 import InquiryModuleService from "../../modules/inquiry/service";
+
+const adminEmail = "serwis.gamefix@gmail.com";
 
 export type CreateInquiryStepInput = {
   name: string;
@@ -39,6 +43,32 @@ export const createInquiryWorkflow = createWorkflow(
   "create-inquiry",
   (input: CreateInquiryWorkflowInput) => {
     const inquiry = createInquiryStep(input);
+
+    const notifications = transform({ inquiry }, ({ inquiry }) => {
+      const storeUrl = "https://gamerfix.pl";
+      const adminUrl = "https://api.gamerfix.pl/app/inquiries";
+
+      return [
+        {
+          to: adminEmail,
+          channel: "email",
+          template: "inquiry-created",
+          data: {
+            inquiry: {
+              clientName: inquiry.name,
+              date: new Date(inquiry.created_at).toLocaleString("pl-PL", {
+                dateStyle: "long",
+                timeStyle: "short",
+              }),
+              storeUrl,
+              inquiriesUrl: adminUrl,
+            },
+          },
+        },
+      ];
+    });
+
+    sendNotificationsStep(notifications);
 
     return new WorkflowResponse(inquiry);
   },
