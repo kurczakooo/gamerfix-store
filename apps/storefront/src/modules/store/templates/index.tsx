a6f0ffbc3d41bda074f39d7e17b2616df6e8ea70
Damian Karwat <damiankarwat04@gmail.com>
2026-09-15 09:52:12 +0200

    hotfix in services page - drop options picker

--- apps/storefront/src/modules/store/templates/index.tsx
+++ apps/storefront/src/modules/store/templates/index.tsx
@@ -38,7 +38,7 @@ const StoreTemplate = ({
       className="flex flex-col small:flex-row small:items-start py-6 content-container"
       data-testid="category-container"
     >
-      <RefinementList sortBy={sort} />
+      <RefinementList sortBy={sort} service={service} />
       <div className="w-full">
         <div className="mb-8 text-2xl-semi">
           <h1 data-testid="store-page-title">
